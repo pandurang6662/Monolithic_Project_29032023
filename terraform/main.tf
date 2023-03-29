6cf9160293d1e2e400000692007ffc341a9651e1
root <root@ip-172-31-39-227.ap-south-1.compute.internal>
2023-03-29 12:15:10 +0000

    terraform code updated

---
+++ terraform/main.tf
@@ -0,0 +1,10 @@
+resource "aws_instances" "key" {
+ami = "ami-0376ec8eacdf70aae"
+instance_type = "t2.micro"
+vpc_security_group_ids = [aws_security_group.demo-sg.id]
+logs = {
+Name = "project-instance"
+Environment = "prod"
+}
+key_name = "mykeypaire1"
+}
---
+++ terraform/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+region = ""ap-south-1"
+access_key = "AKIAZFK6KUTLCG7TMHVH"
+secret_key = "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"
+}
