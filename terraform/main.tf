61457aa58bf3d34ef5f21a2d49b189ca6e748d38
root <root@ip-172-31-39-227.ap-south-1.compute.internal>
2023-03-29 13:30:22 +0000

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "key" {
 ami = "ami-0376ec8eacdf70aae"
 instance_type = "t2.micro"
 vpc_security_group_ids = [aws_security_group.demo-sg.id]
-logs = {
+tags = {
 Name = "project-instance"
 Environment = "prod"
 }
